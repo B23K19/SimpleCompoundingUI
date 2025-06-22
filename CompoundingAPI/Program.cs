5e6c3421a6cd4004e18d1ec9636731441e3b3e45
B23K19 <boyddolamo@gmail.com>
2025-06-22 17:23:14 +0200

    try

--- CompoundingAPI/Program.cs
+++ CompoundingAPI/Program.cs
@@ -5,15 +5,19 @@ using Pomelo.EntityFrameworkCore.MySql;
 
 
 var builder = WebApplication.CreateBuilder(args);
-
+var connectionString = "server=crossover.proxy.rlwy.net;port=52666;database=railway;user=root;password=dEJQoIVQjOHYSShtWOsyPiVjCIGYMByd;";
+var serverVersion = new MySqlServerVersion(new Version(9, 3, 0));
 
 // Add services to the container.
 builder.Services.AddDbContext<CompoundInterestAppDbContext>(options =>
+    options.UseMySql(connectionString, serverVersion)
+);
+/*builder.Services.AddDbContext<CompoundInterestAppDbContext>(options =>
     options.UseMySql(
         builder.Configuration.GetConnectionString("DefaultConnection"),
         ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))
     )
-);
+);*/
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddEndpointsApiExplorer();
